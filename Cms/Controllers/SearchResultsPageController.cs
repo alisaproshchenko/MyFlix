6b7e2c90bed62749516c47c4330ce55358ba8e0d
Alisa Proshchenko <alisa.proshchenko@valtech.com>
2022-04-13 13:19:49 +0300

    created the page, controller, vm and simple view for search and results

---
+++ Cms/Controllers/SearchResultsPageController.cs
@@ -0,0 +1,24 @@
+﻿using Cms.Models.Pages;
+using Cms.Models.ViewModels;
+using EPiServer.Find;
+using EPiServer.Find.Cms;
+using EPiServer.Find.Framework;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cms.Controllers
+{
+    public class SearchResultsPageController : PageController<SearchResultsPage>
+    {
+        public IActionResult Index(SearchResultsPage page, string query)
+        {
+            var model = new SearchViewModel(page, query);
+            if (string.IsNullOrEmpty(query))
+                return View(model);
+
+            var unifiedSearch = SearchClient.Instance.UnifiedSearchFor(query);
+            model.Results = unifiedSearch.GetResult();
+            return View(model);
+        }
+    }
+}
---
+++ Cms/Models/ViewModels/PageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using EPiServer.Core;
+
+namespace Cms.Models.ViewModels
+{
+    public class PageViewModel<T> : IPageViewModel<T> where T : PageData
+    {
+        public PageViewModel() { }
+
+        public PageViewModel(T currentPage)
+        {
+            CurrentPage = currentPage;
+        }
+        public T CurrentPage { get; private set; }
+        public IContent Section { get; set; }
+    }
+}
