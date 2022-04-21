2955dc46c34afbd7f7323a90e27c738d22ebaeaf
Alisa Proshchenko <alisa.proshchenko@valtech.com>
2022-04-21 14:43:43 +0300

    fixed redirecting from search results page, different styles

--- Cms/Controllers/SearchResultsPageController.cs
+++ Cms/Controllers/SearchResultsPageController.cs
@@ -8,6 +8,7 @@ using EPiServer.Core;
 using EPiServer.Find;
 using EPiServer.Find.Cms;
 using EPiServer.Find.Framework;
+using EPiServer.Find.Framework.Statistics;
 using EPiServer.ServiceLocation;
 using EPiServer.Web.Mvc;
 using Microsoft.AspNetCore.Mvc;
@@ -16,16 +17,33 @@ namespace Cms.Controllers
 {
     public class SearchResultsPageController : PageController<SearchResultsPage>
     {
-        public IActionResult Index(SearchResultsPage page, string query)
+        public IActionResult Index(SearchResultsPage page, string query, int pageCount = 1)
         {
             var model = new SearchViewModel(page, query);
             if (string.IsNullOrEmpty(query))
                 return View(model);
-
-            var unifiedSearch = SearchClient.Instance.UnifiedSearchFor(query);
-            var filtered = unifiedSearch.Filter(x => x.MatchTypeHierarchy(typeof(MovieDetailsPage)));
+            
+            var filtered = SearchClient.Instance
+                .UnifiedSearchFor(query)
+                .Filter(x => x.MatchTypeHierarchy(typeof(MovieDetailsPage)))
+                .Track();
             model.Results = filtered.GetResult();
+            var loss = (model.Results.Count() % model.PageSize);
+            model.PagesQuantity = (model.Results.Count() / model.PageSize);
+            if (loss > 0)
+                model.PagesQuantity++;
+
             return View(model);
+        }
+        public IActionResult NextPage(SearchViewModel model)
+        {
+            model.Page++;
+            return View("Index", model);
+        }
+        public IActionResult PreviousPage(SearchViewModel model)
+        {
+            model.Page--;
+            return View("Index", model);
         }
     }
 }
