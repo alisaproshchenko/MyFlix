e890cb14384e4d3eed126819f2a2e4aab2fd5bb8
Alisa Proshchenko <alisa.proshchenko@valtech.com>
2022-04-26 19:54:26 +0300

    created an ApiController for likes

---
+++ Cms/ApiControllers/LikesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EPiServer;
+using EPiServer.Web.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cms.ApiControllers
+{
+    [ApiController]
+    public class LikesController : ControllerBase
+    {
+        private readonly IContentLoader _contentLoader;
+        private readonly IPageRouteHelper _routeHelper;
+
+        public LikesController(IContentLoader contentLoader, IPageRouteHelper routeHelper)
+        {
+            _contentLoader = contentLoader;
+            _routeHelper = routeHelper;
+        }
+        public IActionResult IncrementLike(Guid contentGuid, string language)
+        {
+            
+            if(!ModelState.IsValid) return BadRequest();
+
+            return Ok();
+        }
+    }
+}
