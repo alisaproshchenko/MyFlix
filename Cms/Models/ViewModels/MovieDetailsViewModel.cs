398f9817bf2f736201471dd3c13d2daecd137600
Alisa Proshchenko <alisa.proshchenko@valtech.com>
2022-04-28 13:01:54 +0300

    created MovieDetailsViewModel

---
+++ Cms/Models/ViewModels/MovieDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Cms.Models.Pages;
+
+namespace Cms.Models.ViewModels
+{
+    public class MovieDetailsViewModel
+    {
+        public MovieDetailsPage CurrentPage { get; set; }
+        public Uri LikesIncrementEndpoint { get; set; }
+    }
+}
